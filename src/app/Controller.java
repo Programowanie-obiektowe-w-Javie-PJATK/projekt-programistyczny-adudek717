27ccf8d1e5b5c345ee7774e6227551e07724c2f5
Jakub Jablonski <jjablonski.it@gmail.com>
2020-06-12 12:31:54 +0200

    Added scroll pane to layout, mapWords function

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Controller {
     @FXML
@@ -18,13 +19,28 @@ public class Controller {
     Button saveBtn;
 
     @FXML
-    public HashMap<String,Integer> mapWords () {
-        return new HashMap<String,Integer>();
+    public void initialize() {
+        outputLbl.setWrapText(true);
+    }
+
+    @FXML
+    public LinkedHashMap<String, Integer> mapWords() {
+        final LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
+        String input = inputField.getText();
+
+        for (String word : input.split(" ")) {
+            if (!result.containsKey(word)) { // Create new entry
+                result.put(word, 1);
+            } else { // Increment value of existing key
+                result.put(word, result.get(word) + 1);
+            }
+        }
+        return result;
     }
 
     @FXML
     public void calc() {
-
+        outputLbl.setText(mapWords().toString());
     }
 
     @FXML
