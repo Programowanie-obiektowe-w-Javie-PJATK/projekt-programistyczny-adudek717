64e04114227bfce64652400c336e0d4c1a825c0b
Jakub Jablonski <jjablonski.it@gmail.com>
2020-06-12 12:16:11 +0200

    Added controller functions

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,4 +1,34 @@
 package app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.util.HashMap;
+
 public class Controller {
+    @FXML
+    TextArea inputField;
+    @FXML
+    Label outputLbl;
+    @FXML
+    Button calcBtn;
+    @FXML
+    Button saveBtn;
+
+    @FXML
+    public HashMap<String,Integer> mapWords () {
+        return new HashMap<String,Integer>();
+    }
+
+    @FXML
+    public void calc() {
+
+    }
+
+    @FXML
+    public void save() {
+
+    }
 }
