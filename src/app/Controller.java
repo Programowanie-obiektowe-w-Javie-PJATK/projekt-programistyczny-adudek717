348ebacc25291e71ba610b75df2520c0db0661ea
Aleksander Dudek <a.dudek717@gmail.com>
2020-06-12 13:59:32 +0200

    Stworzona struktura kontrolera

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,5 +1,44 @@
 package app;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
+import java.io.*;
+import java.util.*;
+
+
+public class Controller {
+    @FXML
+    TextArea inputField;
+    @FXML
+    Label outputLbl;
+    @FXML
+    Button calcBtn;
+    @FXML
+    Button saveBtn;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    public LinkedHashMap<String, Integer> mapWords() {
+        final LinkedHashMap<String, Integer> result = new LinkedHashMap<>();
+
+        return result;
+    }
+
+    @FXML
+    public void calc() {
+
+    }
+
+    @FXML
+    public void save() {
+
+    }
 }
